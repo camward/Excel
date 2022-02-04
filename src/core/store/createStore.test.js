7c219f1af5dad8c82e3c681570cdb5e1960f759f
alex <camward@yandex.ru>
2022-02-04 21:35:39 +0400

    тестирование. настройка окружения

---
+++ src/core/store/createStore.test.js
@@ -0,0 +1,11 @@
+import {createStore} from './createStore'
+
+describe('TEST', () => {
+
+  test('test', () => {
+    const store = createStore(() => {}, {})
+    expect(store).toBeDefined()
+  })
+
+})
+
