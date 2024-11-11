a825ef14e7070a798da90fb0ea444afb84c11417
camward <camward@yandex.ru>
2024-11-11 19:54:10 +0400

    состояние размера ячеек

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,3 +1,16 @@
+import { TABLE_RESIZE } from './types';
+
 export function rootReducer(state, action) {
-  return state;
+  let prevState;
+  let field;
+  console.log(action);
+  switch (action.type) {
+    case TABLE_RESIZE:
+      field = action.data.type === 'col' ? 'colState' : 'rowState';
+      prevState = state[field] || {};
+      prevState[action.data.id] = action.data.value;
+      return { ...state, [field]: prevState };
+    default:
+      return state;
+  }
 }
